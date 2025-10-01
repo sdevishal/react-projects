58ad24c90b9880b973a9dd2dfba427fb80a6723d
sdevishal <vishalmishrasde@gmail.com>
2025-10-01 16:21:23 +0530

    add custom Form fields

---
+++ formControl_project/src/components/CustomFormField.jsx
@@ -0,0 +1,37 @@
+import style from "./ExpenseForm.module.css";
+
+export const Input = ({ label, name, id, value, onChange, error }) => {
+  return (
+    <div className={style["input-group"]}>
+      <label>{label}</label>
+      <input id={id} name={name} value={value} onChange={onChange} />
+      <p className={style["errMsg"]}>{error}</p>
+    </div>
+  );
+};
+
+export const Select = ({
+  label,
+  name,
+  id,
+  value,
+  onChange,
+  error,
+  options,
+  defaultOption,
+}) => {
+  return (
+    <div className={style["input-group"]}>
+      <label>{label}</label>
+      <select name={name} id={id} value={value} onChange={onChange}>
+        <option hidden>{defaultOption}</option>
+        {options.map((option, i) => (
+          <option key={i} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <p className={style["errMsg"]}>{error}</p>
+    </div>
+  );
+};
--- formControl_project/src/components/ExpenseForm.jsx
+++ formControl_project/src/components/ExpenseForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import style from "./ExpenseForm.module.css";
+import { Input, Select } from "./CustomFormField";
 
 const ExpenseForm = ({ setExpenses }) => {
   const [expense, setExpense] = useState({
@@ -7,8 +7,22 @@ const ExpenseForm = ({ setExpenses }) => {
     category: "",
     amount: "",
   });
-  function handleSubmit(e) {
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setExpense((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    setErrors({});
+  };
+
+  const handleSubmit = (e) => {
     e.preventDefault();
+    const validateResult = validateForm(expense);
+    if (Object.keys(validateResult).length) return;
+
     setExpenses((prevState) => [
       ...prevState,
       { ...expense, id: crypto.randomUUID() },
@@ -18,62 +32,53 @@ const ExpenseForm = ({ setExpenses }) => {
       category: "",
       amount: "",
     });
-  }
+  };
+
+  const validateForm = (formData) => {
+    const errMsg = {};
+    if (!formData.title) {
+      errMsg.title = "Title is required";
+    }
+    if (!formData.category) {
+      errMsg.category = "Please Select a Category";
+    }
+    if (!formData.amount) {
+      errMsg.amount = "Amount is required";
+    }
+
+    setErrors(errMsg);
+    return errMsg;
+  };
 
   return (
     <div className="form-input">
       <form onSubmit={handleSubmit}>
-        <div className={style["input-group"]}>
-          <label>Title</label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={expense.title}
-            onChange={(e) =>
-              setExpense((prevState) => ({
-                ...prevState,
-                title: e.target.value,
-              }))
-            }
-          />
-        </div>
-        <div className={style["input-group"]}>
-          <label>Category</label>
-          <select
-            name="category"
-            id="category"
-            value={expense.category}
-            onChange={(e) =>
-              setExpense((prevState) => ({
-                ...prevState,
-                category: e.target.value,
-              }))
-            }
-          >
-            <option hidden>Select Category</option>
-            <option value="Grocery">Grocery</option>
-            <option value="Clothes">Clothes</option>
-            <option value="Bills">Bills</option>
-            <option value="Education">Education</option>
-            <option value="Medicine">Medicine</option>
-          </select>
-        </div>
-        <div className={style["input-group"]}>
-          <label>Amount</label>
-          <input
-            type="text"
-            name="amount"
-            id="amount"
-            value={expense.amount}
-            onChange={(e) =>
-              setExpense((prevState) => ({
-                ...prevState,
-                amount: e.target.value,
-              }))
-            }
-          />
-        </div>
+        <Input
+          label="Title"
+          id="title"
+          name="title"
+          value={expense.title}
+          onChange={handleChange}
+          error={errors.title}
+        />
+        <Select
+          label="Category"
+          id="category"
+          name="category"
+          value={expense.category}
+          onChange={handleChange}
+          error={errors.category}
+          options={["Grocery", "Clothes", "Bills", "Education", "Medicine"]}
+          defaultOption="Select a Category"
+        />
+        <Input
+          label="Amount"
+          id="amount"
+          name="amount"
+          value={expense.amount}
+          onChange={handleChange}
+          error={errors.amount}
+        />
         <button type="submit">Add</button>
       </form>
     </div>
